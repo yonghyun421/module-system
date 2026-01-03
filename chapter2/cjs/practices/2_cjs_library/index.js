8af98107dc8ccbad634f6a94f98f7546275d0b07
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2026-01-04 00:13:58 +0900

    feat: 순환참조를 방지하기 위해 closure 패턴 사용

--- chapter2/cjs/practices/2_cjs_library/index.js
+++ chapter2/cjs/practices/2_cjs_library/index.js
@@ -1,15 +1,19 @@
-process.env.DEBUG = 'http';
-const debug = require('debug')('http')
-const http = require('http')
-const name = 'My App';
+process.env.DEBUG = "http*";
+const debug = require("debug")("http");
+const debug2 = require("debug")("http2");
+const http = require("http");
+const name = "My App";
 
 // fake app
 
-debug('booting %o', name);
+debug("booting %o", name);
+debug2("booting %o", name);
 
-http.createServer(function(req, res) {
-  debug(req.method + ' ' + req.url);
-  res.end('hello\n');
-}).listen(3000, function() {
-  debug('listening');
-});
+http
+  .createServer(function (req, res) {
+    debug(req.method + " " + req.url);
+    res.end("hello\n");
+  })
+  .listen(3000, function () {
+    debug("listening");
+  });
